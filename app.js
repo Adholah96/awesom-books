43269c85b99b8433e33c1bda5846f42d598127bb
Yidnekachew Kassahun Kibret <yamiyekn@gmail.com>
2023-02-13 22:03:44 +0300

    Add modification to the app.js file.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-document.getElementById('myForm').addEventListener('submit', saveBooks);
+document.getElementById("myForm").addEventListener("submit", saveBooks);
 
 function saveBooks(e) {
   // form values
-  const titleName = document.getElementById('titleName').value;
-  const authorName = document.getElementById('authorName').value;
+  const titleName = document.getElementById("titleName").value;
+  const authorName = document.getElementById("authorName").value;
 
   const library = {
     name: titleName,
@@ -12,14 +12,14 @@ function saveBooks(e) {
 
   //   local storage
 
-  if (localStorage.getItem('books') === null) {
+  if (localStorage.getItem("books") === null) {
     const books = [];
     books.push(library);
-    localStorage.setItem('books', JSON.stringify(books));
+    localStorage.setItem("books", JSON.stringify(books));
   } else {
-    const books = JSON.parse(localStorage.getItem('books'));
+    const books = JSON.parse(localStorage.getItem("books"));
     books.push(library);
-    localStorage.setItem('books', JSON.stringify(books));
+    localStorage.setItem("books", JSON.stringify(books));
   }
 
   e.preventDefault();
@@ -27,35 +27,32 @@ function saveBooks(e) {
   fetchLibrary();
 }
 
-const targetBody = document.getElementById('libraryBooks');
+const targetBody = document.getElementById("libraryBooks");
 
 function fetchLibrary() {
-  const getBooks = JSON.parse(localStorage.getItem('books'));
-  const libraryBooks = document.getElementById('libraryBooks');
+  const getBooks = JSON.parse(localStorage.getItem("books"));
+  const libraryBooks = document.getElementById("libraryBooks");
 
-  libraryBooks.innerHTML = '';
+  libraryBooks.innerHTML = "";
 
   for (let i = 0; i < getBooks.length; i++) {
     const { name } = getBooks[i];
     const { title } = getBooks[i];
-    libraryBooks.innerHTML
-      += `<p>${name}</p>
+    libraryBooks.innerHTML += `<p>${name}</p>
                                <p>${title}</p> 
-                               <button onclick="deleteBook(\'${
-  title
-}\')">Remove</button>
+                               <button onclick="deleteBook(\'${title}\')">Remove</button>
                                <hr>`;
   }
 }
 fetchLibrary();
 
 function deleteBook(title) {
-  const books = JSON.parse(localStorage.getItem('books'));
+  const books = JSON.parse(localStorage.getItem("books"));
   for (let i = 0; i < books.length; i++) {
     if (books[i].title === title) {
       books.splice(i, 1);
     }
   }
-  localStorage.setItem('books', JSON.stringify(books));
+  localStorage.setItem("books", JSON.stringify(books));
   fetchLibrary();
 }
