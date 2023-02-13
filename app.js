316e70c7b192be454ca5b7777e9819e0673dda0e
Adholah96 <adholah96@gmail.com>
2023-02-13 23:13:48 +0300

    linters check

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-document.getElementById("myForm").addEventListener("submit", saveBooks);
+function fetchLibrary() {
+  const getBooks = JSON.parse(localStorage.getItem('books'));
+  const libraryBooks = document.getElementById('libraryBooks');
+
+  libraryBooks.innerHTML = '';
+
+  for (let i = 0; i < getBooks.length; i += 1) {
+    const { name } = getBooks[i];
+    const { title } = getBooks[i];
+
+    libraryBooks.innerHTML += `<p>${name}</p>
+                               <p>${title}</p>   
+                               <button onclick="deleteBook(\'${title}\')">Remove</button>
+                               <hr>`;
+  }
+}
+
+// eslint-disable-next-line  no-unused-vars
+function deleteBook(title) {
+  const books = JSON.parse(localStorage.getItem('books'));
+  for (let i = 0; i < books.length; i += 1) {
+    if (books[i].title === title) {
+      books.splice(i, 1);
+    }
+  }
+  localStorage.setItem('books', JSON.stringify(books));
+  fetchLibrary();
+}
 
 function saveBooks(e) {
   // form values
-  const titleName = document.getElementById("titleName").value;
-  const authorName = document.getElementById("authorName").value;
+  const titleName = document.getElementById('titleName').value;
+  const authorName = document.getElementById('authorName').value;
 
   const library = {
     name: titleName,
@@ -12,14 +39,14 @@ function saveBooks(e) {
 
   //   local storage
 
-  if (localStorage.getItem("books") === null) {
+  if (localStorage.getItem('books') === null) {
     const books = [];
     books.push(library);
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   } else {
-    const books = JSON.parse(localStorage.getItem("books"));
+    const books = JSON.parse(localStorage.getItem('books'));
     books.push(library);
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   }
 
   e.preventDefault();
@@ -27,32 +54,7 @@ function saveBooks(e) {
   fetchLibrary();
 }
 
-const targetBody = document.getElementById("libraryBooks");
+document.getElementById('myForm').addEventListener('submit', saveBooks);
+// const targetBody = document.getElementById('libraryBooks');
 
-function fetchLibrary() {
-  const getBooks = JSON.parse(localStorage.getItem("books"));
-  const libraryBooks = document.getElementById("libraryBooks");
-
-  libraryBooks.innerHTML = "";
-
-  for (let i = 0; i < getBooks.length; i++) {
-    const { name } = getBooks[i];
-    const { title } = getBooks[i];
-    libraryBooks.innerHTML += `<p>${name}</p>
-                               <p>${title}</p> 
-                               <button onclick="deleteBook(\'${title}\')">Remove</button>
-                               <hr>`;
-  }
-}
 fetchLibrary();
-
-function deleteBook(title) {
-  const books = JSON.parse(localStorage.getItem("books"));
-  for (let i = 0; i < books.length; i++) {
-    if (books[i].title === title) {
-      books.splice(i, 1);
-    }
-  }
-  localStorage.setItem("books", JSON.stringify(books));
-  fetchLibrary();
-}
