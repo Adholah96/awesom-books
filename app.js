a8ed5496245976f70a197b7eb842b157b063c3f1
Adholah96 <adholah96@gmail.com>
2023-02-15 16:29:16 +0300

    check linters and update README file

--- app.js
+++ app.js
@@ -1,85 +1,85 @@
-const libraryBooks = document.querySelector('[data-book]')
-const titleName = document.getElementById('titleName')
-const authorName = document.getElementById('authorName')
-const form = document.getElementById('myForm')
+const libraryBooks = document.querySelector('[data-book]');
+const titleName = document.getElementById('titleName');
+const authorName = document.getElementById('authorName');
+const form = document.getElementById('myForm');
 
 class Library {
   constructor(id, author, title) {
-    this.id = id
-    this.author = author
-    this.title = title
+    this.id = id;
+    this.author = author;
+    this.title = title;
   }
-  //Local Storage
+  // Local Storage
 
   static addLocalStorage(libraryContainer) {
-    let storage = localStorage.setItem(
+    const storage = localStorage.setItem(
       'books',
-      JSON.stringify(libraryContainer)
-    )
-    return storage
+      JSON.stringify(libraryContainer),
+    );
+    return storage;
   }
 
   static getLocalStorage() {
-    let storage =
-      localStorage.getItem('books') === null
-        ? []
-        : JSON.parse(localStorage.getItem('books'))
-    return storage
+    const storage = localStorage.getItem('books') === null
+      ? []
+      : JSON.parse(localStorage.getItem('books'));
+    return storage;
   }
 
   // display in the DOM
 
   static displayBooks() {
-    let displayData = libraryContainer.map((item) => {
-      return `
+    /* eslint-disable no-use-before-define */
+    const displayData = libraryContainer.map(
+      (item) => `
         <div class='books'>
         <p> "${item.title}" by ${item.author} </p>   
         <button class="delete-btn" data-id= ${item.id}>Remove</button>
         </div>
-        `
-    })
-    libraryBooks.innerHTML = displayData.join(' ')
+        `,
+    );
+    libraryBooks.innerHTML = displayData.join(' ');
   }
 
-  //clear input once submited
+  // clear input once submited
   static clearInput() {
-    titleName.value = ''
-    authorName.value = ''
+    titleName.value = '';
+    authorName.value = '';
   }
 
-  //delete book from DOM and arraylibrary
+  // delete book from DOM and arraylibrary
   static deleteBook() {
     libraryBooks.addEventListener('click', (e) => {
       if (e.target.classList.contains('delete-btn')) {
-        e.target.parentElement.remove()
+        e.target.parentElement.remove();
       }
-      let btnId = e.target.dataset.id
-      Library.removeLibraryArray(btnId)
-    })
+      const btnId = e.target.dataset.id;
+      Library.removeLibraryArray(btnId);
+    });
   }
+
   static removeLibraryArray(id) {
-    libraryContainer = libraryContainer.filter((item) => item.id !== +id)
-    Library.addLocalStorage(libraryContainer)
+    libraryContainer = libraryContainer.filter((item) => item.id !== +id);
+    Library.addLocalStorage(libraryContainer);
   }
 }
 
 // innitialize form submit to create Library instance
 form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  let id = Math.floor(Math.random() * 1000)
-  const book = new Library(id, authorName.value, titleName.value)
-  libraryContainer.push(book)
-  Library.displayBooks()
-  Library.clearInput()
-  Library.deleteBook()
-  Library.addLocalStorage(libraryContainer)
-})
+  e.preventDefault();
+  const id = Math.floor(Math.random() * 1000);
+  const book = new Library(id, authorName.value, titleName.value);
+  libraryContainer.push(book);
+  Library.displayBooks();
+  Library.clearInput();
+  Library.deleteBook();
+  Library.addLocalStorage(libraryContainer);
+});
 
 window.addEventListener('DOMContentLoaded', () => {
-  Library.displayBooks()
-  Library.deleteBook()
-})
+  Library.displayBooks();
+  Library.deleteBook();
+});
 
-//store values in a container referrenced by local storage
-
-let libraryContainer = Library.getLocalStorage()
+// store values in a container referrenced by local storage
+let libraryContainer = Library.getLocalStorage();
