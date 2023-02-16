0460b72d9c96e3e0a6dd9325d9cf6b44a90e5aee
Adholah96 <adholah96@gmail.com>
2023-02-16 11:56:14 +0300

    Final changes

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-const libraryBooks = document.querySelector('[data-book]')
-const titleName = document.getElementById('titleName')
-const authorName = document.getElementById('authorName')
-const form = document.getElementById('myForm')
+const libraryBooks = document.querySelector('[data-book]');
+const titleName = document.getElementById('titleName');
+const authorName = document.getElementById('authorName');
+const form = document.getElementById('myForm');
 class Library {
   constructor(id, author, title) {
-    this.id = id
-    this.author = author
-    this.title = title
+    this.id = id;
+    this.author = author;
+    this.title = title;
   }
+
   // Local Storage
   static addLocalStorage(libraryContainer) {
     const storage = localStorage.setItem(
       'books',
-      JSON.stringify(libraryContainer)
-    )
-    return storage
+      JSON.stringify(libraryContainer),
+    );
+    return storage;
   }
+
   static getLocalStorage() {
-    const storage =
-      localStorage.getItem('books') === null
-        ? []
-        : JSON.parse(localStorage.getItem('books'))
-    return storage
+    const storage = localStorage.getItem('books') === null
+      ? []
+      : JSON.parse(localStorage.getItem('books'));
+    return storage;
   }
+
   // display in the DOM
   static displayBooks() {
     /* eslint-disable no-use-before-define */
@@ -32,70 +34,74 @@ class Library {
         <p> "${item.title}" by ${item.author} </p>
         <button class="delete-btn" data-id= ${item.id}>Remove</button>
         </div>
-        `
-    )
-    libraryBooks.innerHTML = displayData.join(' ')
+        `,
+    );
+    libraryBooks.innerHTML = displayData.join(' ');
   }
+
   // clear input once submited
   static clearInput() {
-    titleName.value = ''
-    authorName.value = ''
+    titleName.value = '';
+    authorName.value = '';
   }
+
   // delete book from DOM and arraylibrar
   static deleteBook() {
     libraryBooks.addEventListener('click', (e) => {
       if (e.target.classList.contains('delete-btn')) {
-        e.target.parentElement.remove()
+        e.target.parentElement.remove();
       }
-      const btnId = e.target.dataset.id
-      Library.removeLibraryArray(btnId)
-    })
+      const btnId = e.target.dataset.id;
+      Library.removeLibraryArray(btnId);
+    });
   }
+
   static removeLibraryArray(id) {
-    libraryContainer = libraryContainer.filter((item) => item.id !== +id)
-    Library.addLocalStorage(libraryContainer)
+    libraryContainer = libraryContainer.filter((item) => item.id !== +id);
+    Library.addLocalStorage(libraryContainer);
   }
+
   static currentDate() {
-    const date = new Date()
-    document.getElementById('dateDisplay').innerHTML = date
+    const date = new Date();
+    document.getElementById('dateDisplay').innerHTML = date;
   }
 }
 // innitialize form submit to create Library instance
 form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const id = Math.floor(Math.random() * 1000)
-  const book = new Library(id, authorName.value, titleName.value)
-  libraryContainer.push(book)
-  Library.displayBooks()
-  Library.clearInput()
-  Library.deleteBook()
-  Library.addLocalStorage(libraryContainer)
-})
+  e.preventDefault();
+  const id = Math.floor(Math.random() * 1000);
+  const book = new Library(id, authorName.value, titleName.value);
+  libraryContainer.push(book);
+  Library.displayBooks();
+  Library.clearInput();
+  Library.deleteBook();
+  Library.addLocalStorage(libraryContainer);
+});
 window.addEventListener('DOMContentLoaded', () => {
-  Library.displayBooks()
-  Library.deleteBook()
-  Library.currentDate()
-})
+  Library.displayBooks();
+  Library.deleteBook();
+  Library.currentDate();
+});
 // store values in a container referrenced by local storage
-let libraryContainer = Library.getLocalStorage()
+let libraryContainer = Library.getLocalStorage();
 // Single Page Application
-const books = document.querySelector('.listone')
-const addNew = document.querySelector('.listtwo')
-const contact = document.querySelector('.listthree')
-const booksContainer = document.getElementById('booksContainer')
-const contactContainer = document.getElementById('contact')
+const books = document.querySelector('.listone');
+const addNew = document.querySelector('.listtwo');
+const contact = document.querySelector('.listthree');
+const booksContainer = document.getElementById('booksContainer');
+const contactContainer = document.getElementById('contact');
 books.addEventListener('click', () => {
-  booksContainer.style.display = 'block'
-  form.style.display = 'none'
-  contactContainer.style.display = 'none'
-})
+  booksContainer.style.display = 'block';
+  form.style.display = 'none';
+  contactContainer.style.display = 'none';
+});
 addNew.addEventListener('click', () => {
-  booksContainer.style.display = 'none'
-  form.style.display = 'block'
-  contactContainer.style.display = 'none'
-})
+  booksContainer.style.display = 'none';
+  form.style.display = 'block';
+  contactContainer.style.display = 'none';
+});
 contact.addEventListener('click', () => {
-  contactContainer.style.display = 'block'
-  booksContainer.style.display = 'none'
-  form.style.display = 'none'
-})
+  contactContainer.style.display = 'block';
+  booksContainer.style.display = 'none';
+  form.style.display = 'none';
+});
